d3c004073f41cbe0f1bcae61b58a3697c5815334
Abhishek Sharma <58878438+imgeekabhi@users.noreply.github.com>
2021-10-05 18:23:39 +0530

    Update year.java
    Added some lines for better understanding!!!

--- java-program/year.java
+++ java-program/year.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 class year {
     public static void main(String[] args) {
         int y;
-        System.out.println("Enter a year");
-        Scanner r = new Scanner(System.in);
-        y = r.nextInt();
+        System.out.println("Enter a year : ");
+        Scanner sc = new Scanner(System.in);
+        y = sc.nextInt();   //To input a year from user
 
-        if (y % 100 == 0 && y % 400 == 0 || y % 100 != 0 && y % 4 == 0) {
-            System.out.print("Leap year");
+        if ((y % 100 == 0) && (y % 400 == 0) || (y % 100 != 0) && (y % 4 == 0)) {
+            System.out.print("YES!! Entered Year is a Leap year");
         } else {
-            System.out.print("Not a leap year");
+            System.out.print("NO!! Entered Year is not a Leap year");
         }
     }
 }
