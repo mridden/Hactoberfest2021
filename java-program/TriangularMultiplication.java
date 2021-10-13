3641bf471984b8843afd93936b3c9737dda0b55f
Harshit Kr Vishwakarma <vharshitkr01gmail.com>
2021-10-13 09:51:34 +0530

    Added a java programs that displays triangular multiplication table.

---
+++ java-program/TriangularMultiplication.java
@@ -0,0 +1,13 @@
+public class TriangularMultiplication {
+    public static void main(String[] args) {
+        outer: for(int i=0; i<10; i++){
+            for(int j=0; j<10; j++){
+                if (j>i){
+                    System.out.println();
+                    continue outer;
+                }
+                System.out.print(" "+(i*j));
+            }
+        }
+    }
+}
